131f72eec44809958c222923deb8fc763ce6384c
Mahendra Yadav <mahendra@azrisolutions.com>
2018-01-28 23:19:37 +0530

    part - 3, views

---
+++ mysite/polls/views.py
@@ -0,0 +1,45 @@
+# from django.shortcuts import render
+from django.http import HttpResponse  # , Http404
+#  from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+
+# version - 1
+#  def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
+
+# version - 2 using render shortcut
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# version - 1
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+# version - 2 using shortcut
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
